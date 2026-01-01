8b88274e38dfd582c146ea4495e5a6f23d8e211a
Kamendra Kushwah <kamendra.gla_cs23@gla.ac.in>
2026-01-01 09:54:02 +0530

    Update recursion.java
    TC : (2^n)

--- CP/DP/Leetcode/Fabonacci Number/recursion.java
+++ CP/DP/Leetcode/Fabonacci Number/recursion.java
@@ -1,1 +1,11 @@
+class FibonacciRecursion {
+    public static int fib(int n) {
+        if (n <= 1) return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+    public static void main(String[] args) {
+        System.out.println(fib(10)); // Example
+    }
+}
 
+// Time complexity : O(
